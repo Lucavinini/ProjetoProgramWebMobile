fd3d0df952b1467bf841da5d237a75e127a8d97b
Dias <130758526+chamanodias@users.noreply.github.com>
2025-04-11 18:15:57 -0300

    Correção LoginPage.jsx

--- ToDoLister/src/pages/LoginPage.jsx
+++ ToDoLister/src/pages/LoginPage.jsx
@@ -5,25 +5,15 @@ import "./LoginPage.css";
 const LoginPage = () => {
   const [usuario, setUsuario] = useState('');
   const [senha, setSenha] = useState('');
-  const [mensagem, setMensagem] = useState('');
   const navigate = useNavigate();
 
-  const handleLogin = async (e) => {
+  const handleLogin = (e) => {
     e.preventDefault();
 
     if (usuario && senha) {
       navigate('/AreaDeTrabalho');
     } else {
       alert('Preencha todos os campos!');
-    }
-    
-    try {
-      await Parse.User.logIn(usuario, senha);
-      setMensagem('Login realizado com sucesso!');
-      navigate('/AreaDeTrabalho');
-    } catch (error) {
-      console.error('Erro ao logar:', error);
-      setMensagem('Usuário ou senha inválidos!');
     }
   };
 
