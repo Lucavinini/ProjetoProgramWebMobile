bd74edb2d412a77690aa766eca8de416fdad7f6d
Lucas <vinicioslucas37@hotmail.com>
2025-04-11 17:08:26 -0300

    Usando o parse para integrar com o backend

---
+++ ToDoLister/src/parseConfig.js
@@ -0,0 +1,6 @@
+import Parse from 'parse';
+
+Parse.initialize('xoO0kEK4mNqSagKHV5UR6YpOAasGE3aC5N9wVyeJ', 'kFnI5qnGp3b670lUtiU3EYfiWr36iDF8w4q3l79K');
+Parse.serverURL = 'https://parseapi.back4app.com';
+
+export default Parse;
